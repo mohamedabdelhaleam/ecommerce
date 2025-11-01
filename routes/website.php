5a9673df68764b3b32d6e44ab551dcc98be3a653
Mohamed Abdelhaleam <mohamedabdelhaleam13@gmail.com>
2025-11-01 11:56:05 +0200

    Add AboutController and route for the About page

--- routes/website.php
+++ routes/website.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\Website\AboutController;
 use App\Http\Controllers\Website\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomeController::class)->name('home');
+Route::get('/about', AboutController::class)->name('about');
